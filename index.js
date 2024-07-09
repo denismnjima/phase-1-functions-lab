8d8075a700ef6c23fb5dc98216a13112f374b31f
denismnjima <denismnjima@gmail.com>
2024-07-09 14:28:23 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    return Math.abs(distance - 42)
+}
+function distanceFromHqInFeet(distance){
+    return Math.abs((distance-42)*264)
+}
+function distanceTravelledInFeet(start,destination){
+    return Math.abs((destination-start)*264)
+
+}
+function calculatesFarePrice(start,destination){
+    let distance = Math.abs((destination-start)*264)
+    if(distance<400) return 0
+    else if(distance<2000) return ((distance-400)*2)/100
+    else if(distance<2500) return 25
+    else return('cannot travel that far')
+
+
+}
